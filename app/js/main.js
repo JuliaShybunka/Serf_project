b8aec8181df10fb548fca96ddd5d29c70a50162e
Julia Shybunka <julishybunka@gmail.com>
2020-05-26 14:02:17 +0300

    Add surf-slider section

--- app/js/main.js
+++ app/js/main.js
@@ -14,5 +14,12 @@ $(function(){
         slidesToShow: 4,
     });
 
+    $('.surf-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="../img/arrows-left.svg"></img>',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="../img/arrows-right.svg"></img>',
+    })
+
   
 });
